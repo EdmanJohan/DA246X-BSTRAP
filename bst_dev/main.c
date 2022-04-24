ea943cedbb6cdfb365f414506e2452992ff90138
Johan Edman <22528843+EdmanJohan@users.noreply.github.com>
2022-04-24 22:40:04 +0200

    Updated makefiles.

--- bst_dev/main.c
+++ bst_dev/main.c
@@ -1,9 +1,19 @@
-#include <stdio.h>
+/**
+ * @file main.c
+ * @author Johan Edman (jedma@kth.com)
+ * @brief DA246X Bootstrapper client
+ * @version 0.1
+ * @date 2022-04-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
+#include <stdio.h>
 #include "include/net_info.h"
 
 int main(void) {
-    puts("RIOT network stack example application");
+    puts("DA246X Bootstrapper | DEVICE");
 
     _print_hw_addr();
     _print_ip_addr();
---
+++ bst_gw/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @author Johan Edman (jedma@kth.se)
+ * @brief DA246X Bootstrapper server
+ * @version 0.1
+ * @date 2022-04-24
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    puts("DA246X Bootstrapper | SERVER");
+
+}
